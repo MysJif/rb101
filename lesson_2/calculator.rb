b69582442941b74c1448b528c8dfc935970ece32
Jake Fields <jakecfields@gmail.com>
2020-09-04 15:54:22 -0500

    Finished calculator bonus features

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,38 +1,46 @@
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
+
 def prompt(message)
   puts("=> #{message}")
 end
 
 def valid_number?(num)
-  num.to_i != 0
+  /^-?\.?\d+?\.*\d*$/.match(num)
 end
 
 def operation_to_message(op)
-  case op
-  when '1'
-    'Adding'
-  when '2'
-    'Subtracting'
-  when '3'
-    'Multiplying'
-  when '4'
-    'Dividing'
-  end
+  word = case op
+         when '1'
+           messages 'add'
+         when '2'
+           messages 'sub'
+         when '3'
+           messages 'mul'
+         when '4'
+           messages 'div'
+         end
+  word
 end
 
-prompt "Welcome to Calculator! Enter your name:"
+prompt messages 'welcome'
 
 name = ''
 loop do
   name = gets.chomp
 
   if name.empty?()
-    prompt "Make sure to use a valid name."
+    prompt messages 'valid_name'
   else
     break
   end
 end
 
-prompt "Hi #{name}!"
+prompt "#{messages 'hi'} #{name}!"
 
 loop do # main loop
   number1 = ''
@@ -40,64 +48,56 @@ loop do # main loop
   operator = ''
 
   loop do
-    prompt "What's the first number?"
+    prompt messages 'first_num'
     number1 = gets.chomp
 
     if valid_number?(number1)
       break
     else
-      prompt "Hmm... that doesn't look like a valid number."
+      prompt messages 'invalid_num'
     end
   end
 
   loop do
-    prompt "What's the second number?"
+    prompt messages 'second_num'
     number2 = gets.chomp
 
     if valid_number?(number2)
       break
     else
-      prompt "Hmm... that doesn't look like a valid number."
+      prompt messages 'invalid_num'
     end
   end
 
-  operator_prompt = <<-MSG
-    What operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
-  MSG
-
-  prompt operator_prompt
+  prompt messages 'what_op'
 
   loop do
     operator = gets.chomp
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt "Must choose 1, 2, 3, or 4."
+      prompt messages 'invalid_op'
     end
   end
 
-  prompt "#{operation_to_message(operator)} the two numbers..."
+  prompt "#{operation_to_message(operator)} #{messages 'op_msg'}"
 
   result = case operator
            when '1'
-             number1.to_i + number2.to_i
+             number1.to_f + number2.to_f
            when '2'
-             number1.to_i - number2.to_i
+             number1.to_f - number2.to_f
            when '3'
-             number1.to_i * number2.to_i
+             number1.to_f * number2.to_f
            when '4'
              number1.to_f / number2.to_f
            end
 
-  prompt "The result is #{result}"
+  prompt "#{messages 'result'} #{result}"
 
-  prompt "Do you want to perform another calculation? (Y to calculate again)"
+  prompt messages 'again'
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt "Thank you for using Calculator! Good bye!"
+prompt messages 'goodbye'
