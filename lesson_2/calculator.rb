0680490608706cc4822a949d51598deb7d2fe9a0
Jake Fields <jakecfields@gmail.com>
2020-09-04 13:23:01 -0500

    Fixed calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -7,7 +7,7 @@ def valid_number?(num)
 end
 
 def operation_to_message(op)
-  case op 
+  case op
   when '1'
     'Adding'
   when '2'
@@ -82,17 +82,16 @@ loop do # main loop
 
   prompt "#{operation_to_message(operator)} the two numbers..."
 
-
   result = case operator
-          when '1'
-            number1.to_i + number2.to_i
-          when '2'
-            number1.to_i - number2.to_i
-          when '3'
-            number1.to_i * number2.to_i
-          when '4'
-            number1.to_f / number2.to_f
-          end
+           when '1'
+             number1.to_i + number2.to_i
+           when '2'
+             number1.to_i - number2.to_i
+           when '3'
+             number1.to_i * number2.to_i
+           when '4'
+             number1.to_f / number2.to_f
+           end
 
   prompt "The result is #{result}"
 
