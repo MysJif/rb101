4d8138bdcd92a428b64ef2bffc31ffcad9d764b6
Jake Fields <jakecfields@gmail.com>
2020-09-29 14:08:44 -0500

    Finishing touches on Twenty One

--- lesson_6/twenty_one/twenty_one.rb
+++ lesson_6/twenty_one/twenty_one.rb
@@ -2,14 +2,18 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
-def init_deck # Returns a fresh deck
-  deck = [['C', 'A'], ['C', '2'], ['C', '3'], ['C', '4'], ['C', '5'], ['C', '6'], ['C', '7'], ['C', '8'], ['C', '9'], ['C', 'J'], ['C', 'Q'], ['C', 'K'], 
-          ['D', 'A'], ['D', '2'], ['D', '3'], ['D', '4'], ['D', '5'], ['D', '6'], ['D', '7'], ['D', '8'], ['D', '9'], ['D', 'J'], ['D', 'Q'], ['D', 'K'],
-          ['H', 'A'], ['H', '2'], ['H', '3'], ['H', '4'], ['H', '5'], ['H', '6'], ['H', '7'], ['H', '8'], ['H', '9'], ['H', 'J'], ['H', 'Q'], ['H', 'K'],
-          ['S', 'A'], ['S', '2'], ['S', '3'], ['S', '4'], ['S', '5'], ['S', '6'], ['S', '7'], ['S', '8'], ['S', '9'], ['S', 'J'], ['S', 'Q'], ['S', 'K']]
+# rubocop:disable Layout/LineLength
+# Returns a fresh deck
+def init_deck
+  [['C', 'A'], ['C', '2'], ['C', '3'], ['C', '4'], ['C', '5'], ['C', '6'], ['C', '7'], ['C', '8'], ['C', '9'], ['C', 'J'], ['C', 'Q'], ['C', 'K'],
+   ['D', 'A'], ['D', '2'], ['D', '3'], ['D', '4'], ['D', '5'], ['D', '6'], ['D', '7'], ['D', '8'], ['D', '9'], ['D', 'J'], ['D', 'Q'], ['D', 'K'],
+   ['H', 'A'], ['H', '2'], ['H', '3'], ['H', '4'], ['H', '5'], ['H', '6'], ['H', '7'], ['H', '8'], ['H', '9'], ['H', 'J'], ['H', 'Q'], ['H', 'K'],
+   ['S', 'A'], ['S', '2'], ['S', '3'], ['S', '4'], ['S', '5'], ['S', '6'], ['S', '7'], ['S', '8'], ['S', '9'], ['S', 'J'], ['S', 'Q'], ['S', 'K']]
 end
+# rubocop:enable Layout/LineLength
 
-def deal_cards!(hand, deck) # Mutates both hand and deck
+# Mutates both hand and deck
+def deal_cards!(hand, deck)
   card = nil
   if hand.empty?
     2.times do
@@ -23,8 +27,9 @@ def deal_cards!(hand, deck) # Mutates both hand and deck
     hand << card
   end
 end
-    
-def hand_value(hand) # Returns an integer value of a hand
+
+# Returns an integer value of a hand
+def hand_value(hand)
   value = 0
   hand.each do |card|
     if card[1] == 'A'
@@ -43,11 +48,13 @@ def hand_value(hand) # Returns an integer value of a hand
   value
 end
 
-def busted?(hand) # Returns a boolean
+# Returns a boolean
+def busted?(hand)
   hand_value(hand) > 21
 end
 
-def parse_card(card) # Returns a string interpreation of card array
+# Returns a string interpreation of card array
+def parse(card)
   suit = ''
   value = ''
   case card[0]
@@ -64,18 +71,20 @@ def parse_card(card) # Returns a string interpreation of card array
   when 'A' then value = 'Ace'
   else value = card[1]
   end
-
+  
   "#{value} of #{suit}"
 end
 
-def display_hand(hand, player) # Displays a provided hand
+# Displays a provided hand
+def display_hand(hand, player)
   prompt "#{player} hand is: (#{hand_value(hand)})"
   hand.each do |card|
-    puts parse_card(card)
+    puts parse(card)
   end
 end
 
-def player(hand, deck) # Player hit / stand loop
+# Player hit / stand loop
+def player(hand, deck)
   answer = nil
   loop do
     prompt 'Would you like to hit or stand?'
@@ -88,14 +97,13 @@ def player(hand, deck) # Player hit / stand loop
 
   if busted?(hand)
     prompt "You busted! Dealer wins."
-    gets
   else
     prompt 'You chose to stand.'
-    gets
   end
 end
 
-def dealer(hand, deck) # Dealer loop
+# Dealer loop
+def dealer(hand, deck)
   loop do
     break if hand_value(hand) >= 17 || busted?(hand)
     deal_cards!(hand, deck)
@@ -103,14 +111,11 @@ def dealer(hand, deck) # Dealer loop
 
   if busted?(hand)
     prompt "Dealer busted. You win!"
-    gets
-  else
-    prompt "Dealer reached limit (#{hand_value(hand)})."
-    gets
   end
 end
 
-def game # Main game loop
+# Main game
+def game
   deck = init_deck
   player_hand = []
   dealer_hand = []
@@ -119,7 +124,7 @@ def game # Main game loop
   deal_cards!(dealer_hand, deck)
 
   prompt "Dealer's face-up card is: (#{hand_value(dealer_hand[-1..-1])})"
-  puts parse_card(dealer_hand[-1])
+  puts parse(dealer_hand[-1])
 
   puts ''
 
@@ -148,8 +153,10 @@ end
 
 loop do
   system 'clear'
-  game()
+  game
   prompt "Would you like to play again?"
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
+
+prompt "Thanks for playing!"
